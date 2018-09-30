39710ee3d94a580a34ba9e4299f187124bf7a986
Ilana Quintino da Silva <ilana_silva@hotmail.com>
2018-09-30 16:50:06 -0300

    Initial code

---
+++ HappyCalendarCore/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HappyCalendarCore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: /Category/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
