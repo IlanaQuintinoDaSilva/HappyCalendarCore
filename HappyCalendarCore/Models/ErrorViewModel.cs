46ec1d35e516ee56d67c6ca56f152e44cc27c796
Ilana Quintino da Silva <ilana_silva@hotmail.com>
2018-09-30 16:05:46 -0300

    Add project files.

---
+++ HappyCalendarCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HappyCalendarCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
