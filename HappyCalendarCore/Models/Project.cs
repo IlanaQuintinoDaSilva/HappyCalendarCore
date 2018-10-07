8561cd9b80490e8bc018fba3a72d2c2c97d521cc
Ilana Quintino da Silva <ilanaquintinodasilva@gmail.com>
2018-10-07 16:46:04 -0300

    Data annotation.

--- HappyCalendarCore/Models/Project.cs
+++ HappyCalendarCore/Models/Project.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HappyCalendarCore.Models
 {
     public class Project
     {
         public int id { get; set; }
+        [Display(Name = "Curso")]
         public string name { get; set; }
+        [Display(Name = "Descrição")]
         public string description { get; set; }
+        [Display(Name = "Valor")]
+        [Column(TypeName = "decimal(18, 2)")]
         public double value { get; set; }
     }
 }
