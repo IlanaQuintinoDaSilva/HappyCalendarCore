7d3031d69c98bd920b474179a9a7402c4b338e6b
Ilana Quintino da Silva <ilanaquintinodasilva@gmail.com>
2018-11-04 14:55:13 -0200

    Addition of filter

--- HappyCalendarCore/Models/Category.cs
+++ HappyCalendarCore/Models/Category.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HappyCalendarCore.Models
 {
     public class Category
     {
         public int ID { get; set; }
+        [Display(Name = "Category")]
         public string Name { get; set; }
     }
 }
